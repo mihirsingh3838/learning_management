c655afefc9be3873571263118fab17dab9996b7a
Mihir Singh <mihirsingh252001@gmail.com>
2024-03-19 01:47:10 +0530

    Backend setup and complete authentication

---
+++ server/app.js
@@ -0,0 +1,42 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import userRoutes from "./router/userRoutes.js";
+import errorMiddleWare from "./middlewares/error.middleware.js";
+
+const app = express();
+
+app.use(express.json());
+
+// for cross origin request issues
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+
+// for parsing the cookies
+app.use(cookieParser());
+
+// middleware dev dependency to see log of server
+app.use(morgan('dev'));
+
+// Route to just test the server
+app.use('/ping', (req, res)=>{
+    res.send('Pong');
+})
+
+// Routing for all user options
+app.use('/api/v1/user', userRoutes)
+
+
+// Routing for all other non existing routes
+app.all('*', (req, res)=>{
+    res.status(404).send("OOPS!! 404 PAGE NOT FOUND");
+})
+
+app.use(errorMiddleWare);
+
+export default app;
