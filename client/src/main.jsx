b04d1313c704bc746cd8f6b97422e8291945948f
Mihir Singh <mihirsingh252001@gmail.com>
2024-03-22 05:35:07 +0530

    Setting up the dependencies

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,18 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
+import { Provider } from 'react-redux'
+
+import App from './App.jsx'
+import store from './Redux/store.js'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+            <Toaster />
+        </BrowserRouter>
+    </Provider>
 )
