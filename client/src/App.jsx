ba28c8af4fc838d9ea3e6eea9319b9c8355395d9
Mihir Singh <mihirsingh252001@gmail.com>
2024-04-03 04:01:02 +0530

    View all courses

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import AboutUs from './Pages/AboutUs'
 import NotFound from './Pages/NotFound'
 import SignUp from './Pages/SignUp'
 import Login from './Pages/Login'
+import CourseList from './Pages/Course/CourseList'
 
 function App() {
   return (
@@ -12,8 +13,11 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage/>}></Route>
         <Route path='/about' element={<AboutUs/>}></Route>
+
         <Route path='/signup' element={<SignUp/>}></Route>
         <Route path='/login' element={<Login/>}></Route>
+
+        <Route path='/courses' element={<CourseList/>}></Route>
 
         <Route path='*' element={<NotFound/>}></Route>
       </Routes>
