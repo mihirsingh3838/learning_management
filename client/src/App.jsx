8e10e54fcfd6ef79b7d89cca2d74fadd1ae468a3
Mihir Singh <mihirsingh252001@gmail.com>
2024-04-04 03:26:41 +0530

    Course description page and authorization

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,24 +8,32 @@ import Login from './Pages/Login'
 import CourseList from './Pages/Course/CourseList'
 import Contact from './Pages/Contact'
 import Denied from './Pages/Denied'
+import CourseDescription from './Pages/Course/CourseDescription'
+import RequireAuth from './Components/Auth/RequireAuth'
+import CreateCourse from './Pages/Course/CreateCourse'
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<HomePage/>}></Route>
-        <Route path='/about' element={<AboutUs/>}></Route>
+        <Route path='/' element={<HomePage />}></Route>
+        <Route path='/about' element={<AboutUs />}></Route>
 
-        <Route path='/signup' element={<SignUp/>}></Route>
-        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/signup' element={<SignUp />}></Route>
+        <Route path='/login' element={<Login />}></Route>
 
-        <Route path='/courses' element={<CourseList/>}></Route>
+        <Route path='/courses' element={<CourseList />}></Route>
+        <Route path='/course/description' element={<CourseDescription />}></Route>
 
-        <Route path='/contact' element={<Contact/>}></Route>
+        <Route path='/contact' element={<Contact />}></Route>
 
-        <Route path='/denied' element={<Denied/>}></Route>
+        <Route path='/denied' element={<Denied />}></Route>
 
-        <Route path='*' element={<NotFound/>}></Route>
+        <Route element={<RequireAuth allowedRoles={["ADMIN"]} />}>
+          <Route path='/course/create' element={<CreateCourse />}></Route>
+        </Route>
+
+        <Route path='*' element={<NotFound />}></Route>
       </Routes>
     </>
   )
