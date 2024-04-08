ecbf1abdd7101759905b4727a9c7edbfd11e4a72
Mihir Singh <65151332+mihirsingh3838@users.noreply.github.com>
2024-04-09 02:32:44 +0530

    Update axiosInstance.js

--- client/src/helpers/axiosInstance.js
+++ client/src/helpers/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL= "http://localhost:4000/api/v1";
+const BASE_URL= "https://learning-management-rho.vercel.app/";
 
 const axiosInstance= axios.create();
 
